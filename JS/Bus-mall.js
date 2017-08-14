9ca01558c3e826962eaa5056b42040c0fa25c918
FrankSeaman <frankseamen@gmail.com>
2017-08-14 14:55:24 -0700

    testing commit

---
+++ JS/Bus-mall.js
@@ -0,0 +1,9 @@
+function items ( name, url, displayCount, voteCount, id){
+    this.name = name;
+    this.url = url;
+    this.displayCount = displayCount;
+    this.voteCount = voteCount;
+    this.id = id;
+
+}
+var image1 = new item (' bag', ' bag.jpg', ' displayCount', ' voteCount', ' id');
