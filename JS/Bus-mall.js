1aeb68121efc7fb04b5ca243e5e31e584c9a3b3c
FrankSeaman <frankseamen@gmail.com>
2017-08-14 15:56:47 -0700

    save 3

--- JS/Bus-mall.js
+++ JS/Bus-mall.js
@@ -1,9 +1,42 @@
-function items ( name, url, displayCount, voteCount, id){
+function Items ( name, url, displayCount, voteCount, id){
     this.name = name;
     this.url = url;
     this.displayCount = displayCount;
     this.voteCount = voteCount;
     this.id = id;
 
+    this.addToDom();
 }
-var image1 = new item (' bag', ' bag.jpg', ' displayCount', ' voteCount', ' id');
+
+
+    Items.prototype.addToDom = function (){
+        var pic = document.getElementById(pic1).onclick = function (){;
+        var picImg = document.createEvent( 'click');
+        picImg.document.images = this.name;
+        pic.appendChild (picImg);
+    
+}
+
+
+var image1 = new Items (' bag', ' bag.jpg', 0, 0, 'bag');
+var image2 = new Items (' banana', ' banana.jpg', 0, 0, 'banana');
+var image3 = new Items (' bathroom', ' bathroom.jpg', 0, 0, ' bathroom');
+var image4 = new Items (' boots', ' boots.jpg', 0, 0, ' ');
+var image5 = new Items (' breakfast', ' breakfast.jpg', 0, 0, ' ');
+var image6 = new Items (' bubblegum', ' bubblegum.jpg', 0, 0, ' ');
+var image7 = new Items (' chair', ' chair.jpg', 0, 0, ' ');
+var image8 = new Items (' cthulhu', ' cthulhu.jpg', 0, 0, ' ');
+var image9 = new Items (' dog-duck', ' dog-duck.jpg', 0, 0, ' ');
+var image10 = new Items (' dragon', ' dragon.jpg', 0, 0, ' ');
+var image11 = new Items (' pen', ' pen.jpg', 0, 0, ' ');
+var image12 = new Items (' pet-sweep', ' pet-sweep.jpg', 0, 0, ' ');
+var image13 = new Items (' scissors', ' scissors.jpg', 0, 0, ' ');
+var image14 = new Items (' shark', ' shark.jpg', 0, 0, ' ');
+var image15 = new Items (' sweep', ' sweep.png', 0, 0, ' ');
+var image16 = new Items (' tauntaun', ' tauntaun.jpg', 0, 0, ' ');
+var image17 = new Items (' unicorn', ' unicorn.jpg', 0, 0, ' ');
+var image18 = new Items (' usb', ' usb.gif', 0, 0, ' ');
+var image19 = new Items (' water-can', ' water-can.jpg', 0, 0, ' ');
+var image20 = new Items (' wine-glass', ' wine-glass.jpg', 0, 0, ' ');
+
+    }
