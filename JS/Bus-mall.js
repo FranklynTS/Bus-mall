d64c4e02a924117a3f0710019ba4a0d02ff6d569
FrankSeaman <frankseamen@gmail.com>
2017-08-16 12:24:46 -0700

    chart update

--- JS/Bus-mall.js
+++ JS/Bus-mall.js
@@ -12,26 +12,26 @@ function Items(name, url, id) {
 }
 
 
-var image1 = new Items(' bag', 'images/bag.jpg', 'bag');
-var image2 = new Items(' banana', 'images/banana.jpg', 'banana');
-var image3 = new Items(' bathroom', 'images/bathroom.jpg', 'bathroom');
-var image4 = new Items(' boots', 'images/boots.jpg', 'boots');
-var image5 = new Items(' breakfast', 'images/breakfast.jpg', 'breakfast');
-var image6 = new Items(' bubblegum', 'images/bubblegum.jpg', 'bubblegum');
-var image7 = new Items(' chair', 'images/chair.jpg', 'chair');
-var image8 = new Items(' cthulhu', 'images/cthulhu.jpg', 'cthulhu');
-var image9 = new Items(' dog-duck', 'images/dog-duck.jpg', 'dog-duck');
-var image10 = new Items(' dragon', 'images/dragon.jpg', 'dragon');
-var image11 = new Items(' pen', 'images/pen.jpg', 'pen');
-var image12 = new Items(' pet-sweep', 'images/pet-sweep.jpg', 'pet-sweep');
-var image13 = new Items(' scissors', 'images/scissors.jpg', 'scissors');
-var image14 = new Items(' shark', 'images/shark.jpg', 'shark');
-var image15 = new Items(' sweep', 'images/sweep.png', 'sweep');
-var image16 = new Items(' tauntaun', 'images/tauntaun.jpg', 'tauntaun');
-var image17 = new Items(' unicorn', 'images/unicorn.jpg', 'unicorn');
-var image18 = new Items(' usb', 'images/usb.gif', 'usb');
-var image19 = new Items(' water-can', 'images/water-can.jpg', 'water-can');
-var image20 = new Items(' wine-glass', 'images/wine-glass.jpg', 'wine-glass');
+var image1 = new Items('bag', 'images/bag.jpg', 'bag');
+var image2 = new Items('banana', 'images/banana.jpg', 'banana');
+var image3 = new Items('bathroom', 'images/bathroom.jpg', 'bathroom');
+var image4 = new Items('boots', 'images/boots.jpg', 'boots');
+var image5 = new Items('breakfast', 'images/breakfast.jpg', 'breakfast');
+var image6 = new Items('bubblegum', 'images/bubblegum.jpg', 'bubblegum');
+var image7 = new Items('chair', 'images/chair.jpg', 'chair');
+var image8 = new Items('cthulhu', 'images/cthulhu.jpg', 'cthulhu');
+var image9 = new Items('dog-duck', 'images/dog-duck.jpg', 'dog-duck');
+var image10 = new Items('dragon', 'images/dragon.jpg', 'dragon');
+var image11 = new Items('pen', 'images/pen.jpg', 'pen');
+var image12 = new Items('pet-sweep', 'images/pet-sweep.jpg', 'pet-sweep');
+var image13 = new Items('scissors', 'images/scissors.jpg', 'scissors');
+var image14 = new Items('shark', 'images/shark.jpg', 'shark');
+var image15 = new Items('sweep', 'images/sweep.png', 'sweep');
+var image16 = new Items('tauntaun', 'images/tauntaun.jpg', 'tauntaun');
+var image17 = new Items('unicorn', 'images/unicorn.jpg', 'unicorn');
+var image18 = new Items('usb', 'images/usb.gif', 'usb');
+var image19 = new Items('water-can', 'images/water-can.jpg', 'water-can');
+var image20 = new Items('wine-glass', 'images/wine-glass.jpg', 'wine-glass');
 
 
 var randomImage = function () {
@@ -77,13 +77,13 @@ function voteHandler(event) {
     addVotes(clickedEle);
 
     displayImage();
-if ( voteCount === 25){
+//if ( voteCount === 25){
 
 
 
 }
 
-}
+//}
 function addVotes(target) {
     for (i = 0; i < images.length; i++) {
         if (target.src.match(images[i].url)) {
@@ -94,17 +94,24 @@ function addVotes(target) {
         }
     }
 }
+
 displayImage();
+
 var chartCanvas = document.getElementById('chart');
     var totalChart = new Chart (chartCanvas, {
         type: 'bar',
         data: {
-            labels:['Jan', ' Feb', 'March'],
+            labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu','dog-duck' ,
+            'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass' ],
             dataset: [
                 {
                     label: 'votes',
-                    data: [4, 5, 6]
+                    data: ['click']
                 }
             ]
         }
     })
+
+
+
+    //
